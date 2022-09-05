4edd639660fb7fa9660983a90a3ed7dc7e83cb1c
deepakmp444 <deepakmp444@gmail.com>
2022-09-05 16:55:40 +0530

    adding code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import AuthProvider from "./context/authContext";
+import Home from "./Home";
+import Signin from "./Signin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="sign" element={<Signin />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
